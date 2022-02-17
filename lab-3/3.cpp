64831f7839f05984a332a80a794c519529960a8a
Lyoshka Kopytin <73041675+kopytyoshka@users.noreply.github.com>
2022-02-17 23:39:46 +0300

    Create 3.cpp

---
+++ lab-3/3.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <fstream>
+ 
+using namespace std;
+ 
+string arr[1000], result[1000];
+long int n, m, k;
+ 
+void radixSorting(int position)
+{
+    char symb = 'a';
+    int j = 0;
+    for (int i = 0; i < 26; i++)
+    {
+        for (int g = 0; g < n; g++) 
+        {
+            if (arr[g][position] == symb)
+            {
+                result[j] = arr[g];
+                j++;
+            }
+        }
+        symb++;
+    }
+    for (int i = 0; i < n; i++)
+        arr[i] = result[i];
+}
+ 
+int main()
+{
+    ifstream fin("radixsort.in");
+    ofstream fout("radixsort.out");
+    fin >> n >> m >> k;
+    for (int i = 0; i < n; i++)
+        fin >> arr[i];
+    for (int i = m - 1; i >= m - k; i--)
+        radixSorting(i);
+    for (int i = 0; i < n; i++)
+        fout << arr[i] << endl;
+ 
+    fin.close();
+    fout.close();
+ 
+    return 0;
+}
