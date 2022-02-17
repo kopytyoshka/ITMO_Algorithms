8637b35540c81abb81a5da680547dcedd125aa7d
Lyoshka Kopytin <73041675+kopytyoshka@users.noreply.github.com>
2022-02-17 21:18:30 +0300

    Create 3.cpp

---
+++ lab-2/3.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+ 
+ 
+using namespace std;
+ 
+vector <int> arr;
+vector <int> tmp;
+ 
+int64_t inv = 0;
+ 
+void merge(int part1, int middle, int part2)
+{
+    int i = part1;
+    int j = middle;
+    for (int k = part1; k < part2; ++k)
+    {
+        if (j == part2 || (i < middle && arr[i] <= arr[j]))
+        {
+            tmp[k] = arr[i];
+            i++;
+ 
+        }
+        else
+        {
+            tmp[k] = arr[j];
+            j++;
+            inv += middle - i;
+        }
+    }
+ 
+    for (int i = part1; i < part2; ++i)
+    {
+        arr[i] = tmp[i];
+    }
+}
+ 
+void mergeSort(int part1, int part2)
+{
+    if (part2 <= part1 + 1)
+        return;
+    int middle = (part1 + part2) / 2;
+    mergeSort(part1, middle);
+    mergeSort(middle, part2);
+    merge(part1, middle, part2);
+ 
+}
+ 
+int main()
+{
+    int n;
+    ifstream fin("inversions.in");
+    ofstream fout("inversions.out");
+    fin >> n;
+    arr.resize(n);
+    tmp.resize(n);
+    for (int i = 0; i < n; i++) {
+        fin >> arr[i];
+    }
+    mergeSort(0, n);
+    fout << inv;
+    fin.close();
+    fout.close();
+ 
+    return 0;
+}
