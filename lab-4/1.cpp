4346608ad78885f764a8f222dafdf56413d73c71
Lyoshka Kopytin <73041675+kopytyoshka@users.noreply.github.com>
2022-02-17 23:47:10 +0300

    Create 1.cpp

---
+++ lab-4/1.cpp
@@ -0,0 +1,41 @@
+﻿#include <fstream>
+#include <vector>
+ 
+using namespace std;
+ 
+struct stack {
+    vector <int> a;
+    int size = 0;
+    explicit stack(int n) {
+        a.resize(n);
+    }
+    int pop() {
+        return a[size--];
+    }
+    void push(int val) {
+        a[++size] = val;
+    }
+};
+ 
+int main() {
+    ifstream fin("stack.in");
+    ofstream fout("stack.out");
+    long n;
+    fin >> n;
+    char com;
+    long mean;
+    stack a(n);
+    for (int i = 0; i < n; i++) {
+        fin >> com;
+        if (com == '+') {
+            fin >> mean;
+            a.push(mean);
+        }
+        else {
+            fout << a.pop() << endl;
+        }
+    }
+    fin.close();
+    fout.close();
+    return 0;
+}
